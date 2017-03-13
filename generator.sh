be49a9fedd5b2665b4ef93092280590cb53d8856
Jakub Sękowski <sequba@gmail.com>
2017-03-13 12:51:29 +0100

    generator.sh calls both bin/voronoi and bin/cellular one after the other
    Signed-off-by: Jakub Sękowski <sequba@gmail.com>

--- generator.sh
+++ generator.sh
@@ -11,4 +11,10 @@ mkdir -p output
 # ./bin/cellular < output/zones > output/terrain
 # ...
 
-./bin/cellular 0.5 1 2 < test/zones33x33 > output/terrain33x33
+echo 'a' | ./bin/voronoi  # input/output files should goes here instead of being hardcoded
+cat mapa.txt | head -n 1 > output/voronoi90x90 
+cat mapa.txt | tail -n +2 | tr '.' '$' | tr '0123456789' ' ' >> output/voronoi90x90
+./bin/cellular 0.5 1 2 < output/voronoi90x90 > output/cellular90x90
+
+
+#./bin/cellular 0.5 1 2 < test/zones33x33 > output/terrain33x33
