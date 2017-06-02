50365d302b3475a629b25d1b52c187f8ccabe4c9
Radosław Miernik <radekmie@gmail.com>
2017-06-02 23:43:48 +0200

    Exported globals.

--- homm3lua/homm3lua.h
+++ homm3lua/homm3lua.h
@@ -1,7 +1,9 @@
 #ifndef __H3MLUA_H_DEF__
 #define __H3MLUA_H_DEF__
 
-#include "homm3lua_constants.h"
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 #include "homm3lua_helpers.h"
 
 int luaopen_homm3lua (lua_State *L);
