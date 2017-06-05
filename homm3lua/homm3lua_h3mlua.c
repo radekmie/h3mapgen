74d2dd72be69fd2dbfa7188513c808d334166ea1
Radosław Miernik <radekmie@gmail.com>
2017-06-05 21:20:58 +0200

    Unified error messages.

--- homm3lua/homm3lua_h3mlua.c
+++ homm3lua/homm3lua_h3mlua.c
@@ -7,15 +7,15 @@ h3mlua_xyz h3mlua_check_xyz (lua_State *L, int arg) {
 
   lua_getfield(L, arg, "x");
   const int x = lua_tointegerx(L, -1, &isnum);
-  luaL_argcheck(L, isnum, arg, "x is not an integer");
+  luaL_argcheck(L, isnum, arg, "x must be an integer");
 
   lua_getfield(L, arg, "y");
   const int y = lua_tointegerx(L, -1, &isnum);
-  luaL_argcheck(L, isnum, arg, "y is not an integer");
+  luaL_argcheck(L, isnum, arg, "y must be an integer");
 
   lua_getfield(L, arg, "z");
   const int z = lua_tointegerx(L, -1, &isnum);
-  luaL_argcheck(L, isnum, arg, "z is not an integer");
+  luaL_argcheck(L, isnum, arg, "z must be an integer");
 
   lua_pop(L, 3);
 
