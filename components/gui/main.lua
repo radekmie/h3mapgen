d3c049e9da0e081fe6c0543cd5f00f4ef173d5ba
Radosław Miernik <radekmie@gmail.com>
2018-01-17 18:10:47 +0100

    Added more fields.

--- components/gui/main.lua
+++ components/gui/main.lua
@@ -8,7 +8,28 @@ local model = {
     version = 'RoE',
     seed = 0,
     size = 'S',
-    underground = false
+    underground = false,
+    players = {
+        {id = 1, team = 1, computerOnly = false},
+        {id = 2, team = 2, computerOnly = false},
+        {id = 3, team = 3, computerOnly = false},
+        {id = 4, team = 4, computerOnly = false},
+        {id = 5, team = 5, computerOnly = false},
+        {id = 6, team = 6, computerOnly = false},
+        {id = 7, team = 7, computerOnly = false},
+        {id = 8, team = 8, computerOnly = false}
+    },
+    winning = 'Random',
+    water = 'Random',
+    grail = false,
+    towns = 'Random',
+    monsters = 'Random',
+    welfare = 'Random',
+    branching = 'Random',
+    focus = 'Random',
+    transitivity = 'Random',
+    locations = 'Random',
+    zonesize = 'Random'
 }
 
 -- Controls
@@ -56,6 +77,18 @@ local layout = Layout({
     number('seed', 'Seed'),
     choose('size', 'Map size', {'S', 'M', 'L', 'XL'}),
     option('underground', 'Two level map'),
+    -- TODO: Players component.
+    choose('winning', 'Winning condition', {'Random', 'Defeat all your enemies', 'Capture Town', 'Defeat Monster', 'Acquire Artifact or Defeat All Enemies', 'Build a Grail Structure or Defeat All Enemies'}),
+    choose('water', 'Water', {'Random', 'None', 'Low (lakes, seas)', 'Standard (continents)', 'High (islands)'}),
+    option('grail', 'Map contains Grail.'),
+    choose('towns', 'Towns frequency', {'Random', 'Very rare', 'Rare', 'Normal', 'Common', 'Very common'}),
+    choose('monsters', 'Monster Strength', {'Random', 'Very weak', 'Weak', 'Medium', 'Strong', 'Very strong'}),
+    choose('welfare', 'Welfare', {'Random', 'Very poor', 'Poor', 'Medium', 'Rich', 'Very rich'}),
+    choose('branching', 'Branching', {'Random', 'All zones contain as small number of entrances as possible', 'Most zones contain only minimal number of entrances', 'Some zones contain multiple entrances, some not', 'Most zones contain multiple entrances', 'All zones contain multiple entrances'}),
+    choose('focus', 'Challenge focus', {'Random', 'Strong PvP', 'More PvP', 'Balanced', 'More PvE', 'Strong PvE'}),
+    choose('transitivity', 'transitivity', {'Random', 'Strongly mazelike zones', 'More zones containing mazelike style', 'Zones containing various styles', 'More zones containing open terrain', 'Strongly open terrain zones'}),
+    choose('locations', 'Locations frequency', {'Random', 'Very rare', 'Rare', 'Standard', 'Common', 'Very common'}),
+    choose('zonesize', 'Zone size', {'Random', 'Strongly decreased', 'Decreased', 'Standard', 'Increased', 'Strongly increased'}),
     {
         id = 'submit',
         type = 'button',
@@ -69,6 +102,11 @@ local function serialize ()
     for key, _ in pairs(model) do
         local input = layout[key]
         local value
+
+        -- TODO: Players component.
+        if key == 'players' then
+            input = {type = 'text', value = model[key]}
+        end
 
         if input.type == nil then
             value = input.selected.text
