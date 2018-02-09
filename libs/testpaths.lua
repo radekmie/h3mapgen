23311233a122bbe71dd11a20afa8e8315972c240
Jacek 'Fanderman' Leja <33103954+JacekLeja@users.noreply.github.com>
2018-02-09 03:47:28 +0100

    Update testpaths.lua

--- libs/testpaths.lua
+++ libs/testpaths.lua
@@ -9,7 +9,7 @@ end
 
 map[258] = true
 map[514] = true
-path,len = paths.search_path(map,257,259) -- start x = 1, start y = 1, des x = 3, des y = 1
+len,path = paths.search_path(map,{1,1},{3,1}) -- start x = 1, start y = 1, des x = 3, des y = 1
 
 for i = 1,#path do
   print(path[i]%256, math.floor(path[i]/256)%256)
