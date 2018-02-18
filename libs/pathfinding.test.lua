e78a7d3ba078268ea48faab0bc8773e74ee1de37
Jacek 'Fanderman' Leja <33103954+JacekLeja@users.noreply.github.com>
2018-02-18 21:54:31 +0100

    Update pathfinding.test.lua

--- libs/pathfinding.test.lua
+++ libs/pathfinding.test.lua
@@ -9,7 +9,7 @@ end
 
 map[258] = true
 map[514] = true
-len,path = paths.search_path(map,{1,1},{3,1}) -- start x = 1, start y = 1, des x = 3, des y = 1
+len,path = paths.search_path(map,{1,1},{3,1},{144,72}) -- start x = 1, start y = 1, des x = 3, des y = 1
 
 for i = 1,#path do
   print(path[i]%256, math.floor(path[i]/256)%256)
