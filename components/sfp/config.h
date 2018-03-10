d5ea2019c45cbb2f7437b8386a179f33dc5cf497
Krzysztof Piecuch <krzysztof.piecuch@gmail.com>
2018-03-10 19:30:57 +0100

    genetic algorithm

---
+++ components/sfp/config.h
@@ -0,0 +1,18 @@
+#define WALL    '#'
+#define GROUND  '.'
+#define BLANK   '_'
+
+
+#define MAP_UNEXPECTED_CHARACTER        1
+#define PATTERN_UNEXPECTED_CHARACTER    2
+#define MAP_POI_OUT_OF_BOUND            3
+#define PATTERN_POI_OUT_OF_BOUND        4
+
+#define oo      1000000
+
+#define pop_size  100
+#define best_size 15
+#define n_iter 15
+#define mut_prom 50
+// usunąć n_iter i zastąpić czymś lepszym
+
