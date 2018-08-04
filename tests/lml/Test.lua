ba30b7d0dbbf382b93d52e0fbff8c2eda2587347
Unknown <cutterthecat@gmail.com>
2018-08-05 01:02:32 +0200

    LML nie powinien już robić niespójnych MLMLi (#90)

--- tests/lml/Test.lua
+++ tests/lml/Test.lua
@@ -13,9 +13,13 @@ local MLML    = require('LogicMapLayout/MultiLogicMapLayout')
 local MLMLHelper = require('LogicMapLayout/MLMLHelper')
 
 
-local function Test(name)
+local function Test(name, seed)
   local state = ConfigHandler.Read('tests/lml/'..name..'.h3pgm')  
   state.config = ConfigHandler.Read('config.cfg')
+  
+  if seed ~= nil then
+    state.paramsGeneral.seed = seed
+  end
   
   Params.GenerateDetailedParams(state)
   Params.GenerateInitLMLNode(state)
@@ -59,4 +63,7 @@ end
 --Test('A')
 --print('A: OK')
 Test('01')
-print('01: OK')
+--print('01: OK')
+--for i=1,5 do
+--  Test('01', i)
+--end
